e7452f5ff8fcab4af3710e80278766d639fdd1af
Kevin <stradtkt22@gmail.com>
2019-06-05 10:21:18 -0400

    Added the rest of the routes for the todos

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -7,3 +7,21 @@ router.get('/', (req, res) => {
         .then(todos => res.json(todos))
         .catch(err => res.status(400).json({err}));
 });
+
+router.post('/', (req, res) => {
+    const newTodo = {
+        title: req.body.title,
+        body: req.body.body
+    }
+    newTodo.save()
+        .then(todo => res.json(todo))
+        .catch(err => res.status(500).json({err}));
+});
+
+router.delete('/:id', (req, res) => {
+    Todo.findById(req.params.id)
+        .then(todo => res.status(200).json({todo: true}))
+        .catch(err => res.status(500).json({err: false}));
+});
+
+module.exports = router;
