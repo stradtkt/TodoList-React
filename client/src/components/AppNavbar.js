7b5603ab7054b435a0123267bd1adbfe8bfce11f
Kevin <stradtkt22@gmail.com>
2019-06-05 23:17:49 -0400

    Added app navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="AppNavbar">
+        <Navbar color="primary" primary expand="md">
+          <NavbarBrand href="/" style={{color: 'white'}}>reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
